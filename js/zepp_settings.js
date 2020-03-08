813aa21d11cc43b88ba286a414e21398ba2ab4e5
Jens Horing <jens@horing.com>
2020-03-08 20:01:07 +0100

    Update zepp_settings.js

--- js/zepp_settings.js
+++ js/zepp_settings.js
@@ -3,7 +3,7 @@ var JS_Code_Online = 1;
 
 ZEpp.Settings = ZEpp.Settings || {};
 
-ZEpp.Settings.ServerUrl             = "http://localhost:81/ZEpp_Online";
+//ZEpp.Settings.ServerUrl             = "http://localhost:81/ZEpp_Online";
 ZEpp.Settings.ServerUrl             = "http://www.jensovic.de/zepp";
 
 ZEpp.Settings.sessionTimeoutInMSec  = 1000  * 60    * 60     * 24    * 30;
